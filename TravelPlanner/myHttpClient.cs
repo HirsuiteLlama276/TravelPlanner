c55e2676858b184d3cb6462eb587907dbb01583b
HirsuiteLlama276 <kuba99lol@gmail.com>
2024-03-24 22:05:05 +0100

    Added new class for connecting with Google Places API

---
+++ TravelPlanner/myHttpClient.cs
@@ -0,0 +1,31 @@
+using System.Net.Http.Json;
+
+namespace TravelPlanner;
+
+public class myHttpClient: HttpClient
+{
+    private HttpClient _client = new HttpClient();
+    private const string GetApiKey = "";
+
+    public void AddHeader(string name, string value)
+    {
+        _client.DefaultRequestHeaders.Add(name,value);
+    }
+    public myHttpClient()
+    {
+        _client.BaseAddress = new Uri("https://places.googleapis.com/v1/");
+        _client.DefaultRequestHeaders.Add("X-Goog-Api-Key", GetApiKey);
+        _client.DefaultRequestHeaders.Add("accept","application/json");
+    }
+
+
+
+    
+    public async Task<HttpResponseMessage> SearchForHotel(string hotelName)
+    {
+       HttpResponseMessage task = await _client.PostAsJsonAsync("./places:searchText",  new { textQuery = hotelName });
+       return task;
+    }
+
+
+}
