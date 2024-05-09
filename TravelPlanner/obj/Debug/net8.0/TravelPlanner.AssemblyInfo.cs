0aa3fe1eba8ccbfedc2c23dcf971ae9acb0ae4ec
Mateusz <mateusz.cmok7@gmail.com>
2024-05-09 23:02:52 +0200

    Zmiana układu strony

--- TravelPlanner/obj/Debug/net8.0/TravelPlanner.AssemblyInfo.cs
+++ TravelPlanner/obj/Debug/net8.0/TravelPlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55e2676858b184d3cb6462eb587907dbb01583b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d11f7902bb3487253d0ee962366fc4cc334c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
