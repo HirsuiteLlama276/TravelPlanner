514b98f1227a932623ed7fa604a6e6ac7f41338e
HirsuiteLlama276 <kuba99lol@gmail.com>
2024-07-20 22:35:59 +0200

    Corrected Place.razor - now the hotel photo and website will appear.

--- TravelPlanner/myHttpClient.cs
+++ TravelPlanner/myHttpClient.cs
@@ -6,14 +6,11 @@ namespace TravelPlanner;
 public class myHttpClient: HttpClient
 {
     private HttpClient _client = new HttpClient();
-    private const string GetApiKey = "AIzaSyDG4Nl7kgH0kri6TlLd6nsB162XHy-X_A0";
+    private const string GetApiKey = "";
 
     public void AddHeader(string name, string value)
     {
         _client.DefaultRequestHeaders.Add(name,value);
-    }
-    public myHttpClient()
-    {
     }
     
     public async Task<HttpResponseMessage> SearchForHotel(string hotelName)
@@ -21,16 +18,16 @@ public class myHttpClient: HttpClient
         _client.BaseAddress = new Uri("https://places.googleapis.com/v1/");
         _client.DefaultRequestHeaders.Add("X-Goog-Api-Key", GetApiKey);
         _client.DefaultRequestHeaders.Add("accept","application/json");
-        _client.DefaultRequestHeaders.Add("X-Goog-FieldMask", "places.displayName,places.id,places.photos");
+        _client.DefaultRequestHeaders.Add("X-Goog-FieldMask", "places.displayName,places.id,places.photos,places.websiteUri");
        HttpResponseMessage task = await _client.PostAsJsonAsync("./places:searchText",  new { textQuery = hotelName });
        return task;
     }
     public async Task<HttpResponseMessage> SearchForHotelPhotos(string placeId)
     {
-        Console.WriteLine(placeId);
+        //Console.WriteLine(placeId);
         _client.DefaultRequestHeaders.Add("accept","application/json");
-        HttpResponseMessage response = await _client.GetAsync($"https://places.googleapis.com/v1/{placeId}/media?maxHeightPx=1920&maxWidthPx=1080&key={GetApiKey}&skipHttpRedirect=true");
+        HttpResponseMessage task = await _client.GetAsync($"https://places.googleapis.com/v1/{placeId}/media?maxHeightPx=1920&maxWidthPx=1080&key={GetApiKey}&skipHttpRedirect=true");
         //HttpResponseMessage response = await _client.GetAsync("https://places.googleapis.com/v1/places/ChIJNTbAYao6F0cRGih4nWitIy4/photos/ATplDJbZOD5DInnAmH_WkQmMeBJHVooSb8DRbzIKWnYKGF886gZNHBXlENZlSMNgApBXBuXcIE4kTzlDcCf0eWE9fRcsyc2OK1fN4VM2Q-U-LCzMVVsDw7vgK1elV9iA1aFtrVzy77x9cmwPmSW2mEanZLky0nKZsXYqj7PN/media?maxHeightPx=1920&maxWidthPx=1080&key=AIzaSyDG4Nl7kgH0kri6TlLd6nsB162XHy-X_A0&skipHttpRedirect=true");
-        return response;
+        return task;
     }
 }
